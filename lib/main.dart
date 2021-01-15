064718c92726d5fcdb8ecf22cb7a5a9a71d652db
Thiagosousadev <thiagosousadev@gmail.com>
2021-01-15 19:47:57 -0300

    Criando rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
+import './screens/categories_screen.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meals',
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Meals'),
-        backgroundColor: Colors.blue,
+      title: 'Vamos cozinhar?',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
       ),
-      body: Container(
-        child: Text('Meu App'),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
